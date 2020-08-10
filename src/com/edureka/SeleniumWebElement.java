3520d7116bb6507f736b4797e01de2adee19d27e
Telmo Correa <telverneck@hotmail.com>
2020-08-09 23:03:13 -0400

    Some minor changes

--- src/com/edureka/SeleniumWebElement.java
+++ src/com/edureka/SeleniumWebElement.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumWebElement {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
