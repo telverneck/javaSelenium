21ae5d71858890b8da1122d3e6e039f44f0a340e
Telmo Correa <telverneck@hotmail.com>
2020-08-09 20:50:17 -0400

    First script, just testing Amazon search bar and Title Page response

--- src/com/edureka/SeleniumWebElement.java
+++ src/com/edureka/SeleniumWebElement.java
@@ -15,14 +15,25 @@ public class SeleniumWebElement {
 
 		String baseURL = "https://www.amazon.com.br";
 		String searchBar = "twotabsearchtextbox";
+		String searchInput = "ps4";
+		String trueTitle = "Amazon.com.br";
 
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.get(baseURL);
-		driver.findElement(By.id(searchBar)).sendKeys("ps4");
+		driver.findElement(By.id(searchBar)).sendKeys(searchInput);
 		driver.findElement(By.id(searchBar)).submit();
+
+		String title = driver.getTitle();
+
+		if ((title.contains(trueTitle)) && (title.contains(searchInput))) {
+			System.out.print("Teste ok! \n" + title);
+
+		} else
+			System.out.print("Fails " + title);
+
 		driver.quit();
 	}
 
